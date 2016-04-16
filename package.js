15a9545ad3a709a2415b158f0c2af32866a8cf06
Ronen Moldovan <ronen.moldovan@gmail.com>
2016-04-16 23:09:18 +0300

    Added descrption and git reference

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'ronenm:autoform-reference-typeahead',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'A wrapper for of sergeyt:typeahead for autoform of collection with reference to other collection',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:ronenm/meteor-autoform-reference-typeahead.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
